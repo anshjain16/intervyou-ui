3080a468115f90b43f0d7f672f868d902b9a5703
anshjain16 <anshjain0216@gmail.com>
2024-03-16 19:09:28 +0530

    coding question schema created

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import React, { useRef, useState } from "react";
 import { UserButton } from "@clerk/nextjs";
+import { initialProfile } from "@/lib/initial-profile";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const user = await initialProfile();
+  console.log(user.id);
+  // console.log(user?.emailAddresses[0].emailAddress);
   return (
     <div className="bg-zinc-800 h-screen">
       hello world
-      <UserButton></UserButton>
+      <UserButton afterSignOutUrl="/"></UserButton>
     </div>
   );
 };
